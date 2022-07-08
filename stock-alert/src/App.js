1ec9ce748ca64e469c7627603a915afd109c1979
Gimmick <ashwinraviindia@gmail.com>
2022-07-08 20:49:35 +0530

    Navbar

--- stock-alert/src/App.js
+++ stock-alert/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Alert from "./Components/Alerts/Alert";
 import LogIn from "./Components/Login/LogIn";
@@ -12,9 +12,9 @@ function App() {
         <p>Stock Alert</p>
       </div>
       <div className="App-header">
-        <Link className="App-link" to="/">Home</Link>
-        <Link className="App-link" to="/alerts">Alerts</Link>
-        <Link className="App-link" to="/logIn">LogIn/SignUp</Link>
+        <NavLink className="App-link" to="/alerts">Alerts</NavLink>
+        <NavLink className="App-link" to="/">Home</NavLink>
+        <NavLink className="App-link" to="/logIn">LogIn/SignUp</NavLink>
       </div>
       <Routes>
         <Route path="/" exact element={<Home />} />
